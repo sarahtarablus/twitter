16dbce9c0920fa1c1dced45c335238f648d51bd8
sarahTarablus <sarahtarablus@gmail.com>
2021-03-11 12:33:02 -0800

    update

--- server.js
+++ server.js
@@ -2,10 +2,18 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const person = {
+  name: 'jon',
+  lastName: 'doe',
+  age: 44,
+ }
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(person)
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 });
+
+

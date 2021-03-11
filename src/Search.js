de2633fb54711015859e4e377c5b4d3e82d5b8b8
sarahTarablus <sarahtarablus@gmail.com>
2021-03-11 15:07:16 -0800

    update

--- src/Search.js
+++ src/Search.js
@@ -1,9 +1,45 @@
 import React from 'react';
-import Showcase from './Showcase.js';
+import {useState} from 'react';
+// import Showcase from './Showcase.js';
 
 
 
 const Search = (props) => {
+
+  // const characters = [
+  //   {
+  //    name: 'Jon Doe',
+  //    username: '@jondoe',
+  //    date: 'Mon Mar 09 13:35:09',
+  //    content: 'Hello my name is Jon and I am a new user,Hello my name is Jon and I am a new user,Hello my name is Jon and I am a new user,Hello my name is Jon and I am a new user,Hello my name is Jon and I am a new user,Hello my name is Jon and I am a new user'
+  //   },
+   
+  //   {
+  //    name: 'Brad Davis',
+  //    username: '@braddavis',
+  //    date: 'Mon Mar 09 13:35:09',
+  //    content: 'Hello my name is Jon and I am a new user,Hello my name is Jon and I am a new user,Hello my name is Jon and I am a new user,Hello my name is Jon and I am a new user,Hello my name is Jon and I am a new user,Hello my name is Jon and I am a new user'
+  //   },
+   
+  //   {
+  //    name: 'Mona Diaz',
+  //    username: '@monadiaz',
+  //    date: 'Mon Mar 09 13:35:09',
+  //    content: 'Hello my name is Jon and I am a new user,Hello my name is Jon and I am a new user,Hello my name is Jon and I am a new user,Hello my name is Jon and I am a new user,Hello my name is Jon and I am a new user,Hello my name is Jon and I am a new user'
+  //   },
+   
+  //   {
+  //    name: 'Laila Rally',
+  //    username: '@lailarally',
+  //    date: 'Mon Mar 09 13:35:09',
+  //    content: 'Hello my name is Jon and I am a new user,Hello my name is Jon and I am a new user,Hello my name is Jon and I am a new user,Hello my name is Jon and I am a new user,Hello my name is Jon and I am a new user,Hello my name is Jon and I am a new user'
+  //   }
+  //  ]
+  const {people, setPeople} = useState({});
+
+  // const createShowcase = () => {
+    
+  // }
   return (
     <div className='search'>
       <div className='input'>
@@ -14,7 +50,7 @@ const Search = (props) => {
         <button>Content</button>
       </div>
       <div>
-       <Showcase name='jon doe' username='@jondoe' date='Mon Mar 08 13:37:35' content='Hello my name is Jon and I am a new user,Hello my name is Jon and I am a new user,Hello my name is Jon and I am a new user,Hello my name is Jon and I am a new user,Hello my name is Jon and I am a new user,Hello my name is Jon and I am a new user'/>
+       {/* <Showcase name={} username={} date={} content={}/> */}
       </div>
     </div>
   )
